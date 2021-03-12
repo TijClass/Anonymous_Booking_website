48c031c30f0b1a84325cc346d0197de40935a584
AbdelaliELMANTAGUI <alix123e@mail.Com>
2021-03-12 10:04:33 +0100

    set authGuard

--- Codes/controllers/home.controller.php
+++ Codes/controllers/home.controller.php
@@ -2,3 +2,7 @@
 // echo "from controller <br>";
 include "./models/home.model.php";
 include "./views/home.view.php";
+
+if(isset($userId)){
+    echo '<h1 style="color:red"> '.$userId.' </h1>';
+}
--- Codes/middleware/auth.middleware.php
+++ Codes/middleware/auth.middleware.php
@@ -4,24 +4,18 @@ $key = "token";
 $sessionToken = isset($_SESSION[$key])?$_SESSION[$key]:null;
 $cookieToken = isset($_COOKIE[$key])?$_COOKIE[$key]:null;
 $token = null;
+$userId = null;
 if($sessionToken!= null || $cookieToken != null){
     $token = ($sessionToken != null) ? $sessionToken : $cookieToken;
 }else{
-    echo "redirect 1";
-    die;
     header("location: ./login");
 }
 try {
     $data = (object) fetchToken($token);
-    if(!(getUserMac() == $data->mac)){
-        print_r($data);
-        echo "<br>";
-        echo "getmacuser -".getUserMac()."<br>";
-        echo "from token".$data->mac."<br>";
-        echo "redirect 2";
-        die;
+    if(getUserMac() != $data->mac){
         header("location: ./login");
     }
+    $userId = $data->id;
 } catch (Exception $e){
     echo $e->getMessage();
     die;
