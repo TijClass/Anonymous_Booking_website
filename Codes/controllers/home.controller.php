e01c4297a909ab3b175a58e4e9b6c8147cdb070a
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-03-13 15:49:50 +0100

    features class

--- Codes/controllers/home.controller.php
+++ Codes/controllers/home.controller.php
@@ -2,20 +2,26 @@
 require "./models/images.model.php";
 include "./models/home.model.php"; // home model
 require "./models/agent.model.php"; // agent model
-
-// get home view
-include "./views/home.view.php";
+require "./models/features.model.php"; // features
 
 
 // instances 
 $agent = new Agent(); // Agent
 $hom = new Home(); // Home
+$Feature = new Feature(); // Feature
 
 $top3Agents = $agent->getTop3(); // get Top 3 agents
 $homes = $hom->all(); // get all homes
+$features = $Feature->get(1);
 
 echo "<pre>";
-// print_r($top3Agents);
-// echo "<br>";
+print_r($top3Agents);
+echo "<br>";
 print_r($homes);
+echo "<br>";
+print_r($features);
 echo "</pre>";
+
+
+// get home view
+include "./views/home.view.php";
--- Codes/views/home.view.php
+++ Codes/views/home.view.php
@@ -1,5 +1,4 @@
 <!-- Success alert -->
-    <!-- <div style="padding:10px 15px; background-color:green;color:#fff;border-radius:5px;margin-bottom:16px;">
+    <div style="padding:10px 15px; background-color:green;color:#fff;border-radius:5px;margin-bottom:16px;">
         Welcome back, you have access to this page. Enjoy!"
-    </div> -->
-<!-- // echo "from view <br>"; -->
+    </div>
