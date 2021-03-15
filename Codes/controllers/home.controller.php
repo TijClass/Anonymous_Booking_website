27638fc28f4edcfae6a477516ae83fdf0ea66ff2
AbdelaliELMANTAGUI <alix123e@mail.Com>
2021-03-15 11:16:19 +0100

    Add Gellery class

--- Codes/controllers/home.controller.php
+++ Codes/controllers/home.controller.php
@@ -4,6 +4,7 @@ include "./models/home.model.php"; // home model
 require "./models/agent.model.php"; // agent model
 require "./models/feature.model.php"; // features
 require "./models/partner.model.php"; // features
+require "./models/gallery.model.php"; // features
 
 
 // instances 
@@ -11,12 +12,13 @@ $agent = new Agent(); // Agent
 $hom = new Home(); // Home
 $Feature = new Feature(); // Feature
 $Partner = new Partner(); // Feature
+$gallery = new Gallery(); // Gallery
 
 $top3Agents = $agent->getTop3(); // get Top 3 agents
 $homes = $hom->all(); // get all homes
 $features = $Feature->all();
 $partner = $Partner->all();
-
+$galleries = $gallery->all();
 
 // get home view
 include "./views/home.view.php";
