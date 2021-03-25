a9b2f71a2fc8ab1640d71bd962193a11f69ee855
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-03-25 22:50:49 +0100

    Modal popup

--- Codes/views/home.view.php
+++ Codes/views/home.view.php
@@ -243,5 +243,23 @@
         </div>
     </section>
     </footer>
+    <!-- Modal -->
+    <div id="modal">
+        <div class="modal">
+            <div class="left">
+                <img src="./img/modal-1.png" alt="">
+                <img src="./img/modal-2.png" alt="">
+            </div>
+            <div class="right">
+                <h3>NE TARDE PAS</h3>
+                <small>Lisez les termes avant de choisir</small>
+                <p>lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>
+                lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <a href="/contact" class="btn-primary">BOOK WITH ME</a>
+            </div>
+        </div>
+    </div>
+
+
 </body>
 </html>
