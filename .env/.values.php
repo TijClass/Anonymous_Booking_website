95c262f089c104da2585a9b98aa2f0005bf42e5d
AbdelaliELMANTAGUI <alix123e@mail.Com>
2021-03-28 20:47:38 +0100

    setup remote database credentials

--- .env/.values.php
+++ .env/.values.php
@@ -1,7 +1,7 @@
 <?php
 define("ABS_PATH","http://localhost:3000/");
 define("TOKEN_SECRET","tokenSecret");
-define("HOST","localhost");
-define("DB_NAME","booking");
-define("DB_USER","root");
-define("DB_PASSWORD","");
+define("HOST","freedb.tech");
+define("DB_NAME","freedbtech_booking");
+define("DB_USER","freedbtech_anonymousBookingTeam");
+define("DB_PASSWORD","a7sb5gt2m");
