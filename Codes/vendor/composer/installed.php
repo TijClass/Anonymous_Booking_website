fe79fa02440b2d66f18d682b714c3cc8de1f8ec5
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-03-29 09:25:15 +0100

    autoloader

---
+++ Codes/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '48c031c30f0b1a84325cc346d0197de40935a584',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '48c031c30f0b1a84325cc346d0197de40935a584',
+    ),
+    'firebase/php-jwt' => 
+    array (
+      'pretty_version' => 'v5.2.1',
+      'version' => '5.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f42c9110abe98dd6cfe9053c49bc86acc70b2d23',
+    ),
+  ),
+);
