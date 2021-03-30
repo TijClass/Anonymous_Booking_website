af086a6efad444b453bbcfe5ed182ddf62fa0c26
AbdelaliELMANTAGUI <alix123e@mail.Com>
2021-03-30 10:49:03 +0100

    prepare to deploy

---
+++ .env/.values.php
@@ -0,0 +1,7 @@
+<?php
+define("ABS_PATH","http://localhost:3000/");
+define("TOKEN_SECRET","tokenSecret");
+define("HOST","freedb.tech");
+define("DB_NAME","freedbtech_booking");
+define("DB_USER","freedbtech_anonymousBookingTeam");
+define("DB_PASSWORD","a7sb5gt2m");
---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '56be5d7ba080085ef5bdf4bc56374a9b2a17f806',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '56be5d7ba080085ef5bdf4bc56374a9b2a17f806',
+    ),
+    'firebase/php-jwt' => 
+    array (
+      'pretty_version' => 'v5.2.1',
+      'version' => '5.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f42c9110abe98dd6cfe9053c49bc86acc70b2d23',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.3.0',
+      'version' => '6.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4a08cf4cdd2c38d12ee2b9fa69e5d235f37a6dcb',
+    ),
+  ),
+);
